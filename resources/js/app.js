1642e85d9126c5a67c2b4a715ff232abde0fa1cb
yeaminrabbi <yeaminrabbi308@gmail.com>
2022-08-20 18:29:24 +0600

    Show and List in VUE

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+require('./bootstrap')
+
+import { createApp } from 'vue'
+import BlogForm from './components/addBlogForm'
+import BlogList from './components/BlogList'
+
+const blogform = createApp({})
+blogform.component('blog-form', BlogForm)
+blogform.mount('#BolgForm')
+
+const bloglist = createApp({})
+bloglist.component('blog-list', BlogList)
+bloglist.mount('#BolgList')
---
+++ resources/js/components/addBlogForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" id="title" name="title">
+        </div>
+        <div class="form-group">
+            <label for="title">Category</label>
+            <input type="text" class="form-control" id="category" name="category">
+        </div>
+
+        <div class="form-group">
+            <label for="title">Details</label><br>
+            <textarea class="form-group" name="details" id="details" cols="41" rows="5"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
